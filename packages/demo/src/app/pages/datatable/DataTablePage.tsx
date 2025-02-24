829a8515a882e9a7ec2d97e718693238f36c2bd0
Jasper Oosthoek <jasperoosthoek@hotmail.com>
2025-02-24 18:24:49 +0100

    More work on DataTablePage demo

--- packages/demo/src/app/pages/datatable/DataTablePage.tsx
+++ packages/demo/src/app/pages/datatable/DataTablePage.tsx
@@ -4,6 +4,9 @@ import {
   FormCreateModalButton,
   FormModalProvider,
   useLocalStorage,
+  DeleteConfirmButton,
+  ConfirmButton,
+  ResetButton,
 } from '@jasperoosthoek/react-toolbox';
 
 import usersBase, { User } from './users';
@@ -14,29 +17,10 @@ const DataTablePage = () => {
   // Store users in localStorage so it persists
   const [users, setUsers] = useLocalStorage('users', usersBase);
   // const { lang } = useLocalization();
+  
+  const CreateUserButton = () => <FormCreateModalButton title='Create new user'/>
   return (
     <Container className='container-list'>
-      <h2>DataTable Component</h2>
-      <p>
-        This is a <strong>highly customizable and interactive DataTable component</strong>.
-        It provides a seamless user experience with powerful features, making it ideal for displaying and managing data efficiently while keeping development time to a minimum.
-      </p>
-
-      <h3>Key Features</h3>
-      <ul>
-        <li><strong>Column Sorting</strong> – Click column headers to sort data in ascending or descending order.</li>
-        <li><strong>Pagination</strong> – Navigate through large datasets smoothly with built-in pagination.</li>
-        <li><strong>Drag & Drop</strong> – Easily reorder rows or columns via intuitive drag-and-drop functionality.</li>
-        <li><strong>Row Click to Edit</strong> – Open a modal with an edit form when clicking a row for quick updates.</li>
-        <li><strong>Search & Filter</strong> – Instantly find relevant data with a real-time search input.</li>
-      </ul>
-
-      <p>
-        This component is designed for <strong>performance, accessibility, and flexibility</strong>,
-        making it a powerful solution for handling complex data visualization in any React application. 🚀
-      </p>
-
-      
       <FormModalProvider
         loading={isLoading}
         initialState={{
@@ -61,9 +45,19 @@ const DataTablePage = () => {
             label: 'Role'
           }
         }}
+        validate={({ email }) => {
+          // You can add more sophisticated validation here
+          const es = email.split('@')
+          if (es.length !== 2) {
+            return {
+              email: 'Email address should contain at least one @',
+            }
+          }
+        }}
         onCreate={(user, closeModal: () => void) => {
           setUsers([...users, user as User]);
 
+          // Fake 500 ms loading time in the backend
           setIsLoading(true);
           setTimeout(() => {
             setIsLoading(false);
@@ -72,7 +66,8 @@ const DataTablePage = () => {
         }}
         onUpdate={(user, closeModal: () => void) => {
           setUsers(users.map((u: User) => u.id === user.id ? user as User : u));
-
+          
+          // Fake 500 ms loading time in the backend
           setIsLoading(true);
           setTimeout(() => {
             setIsLoading(false);
@@ -80,6 +75,35 @@ const DataTablePage = () => {
           }, 500);
         }}
       >
+        <h2>The DataTable component</h2>
+        <p>
+          This is a <strong>highly customizable and interactive DataTable component</strong>.
+          It provides a seamless user experience with powerful features, making it ideal for displaying and managing data efficiently while keeping development time to a minimum.
+        </p>
+
+        <h3>Key Features</h3>
+        <ul>
+          <li><strong>Column Sorting</strong> – Click column headers to sort data in ascending or descending order.</li>
+          <li><strong>Pagination</strong> – Navigate through large datasets smoothly with built-in pagination.</li>
+          <li><strong>Drag & Drop</strong> – Easily reorder rows or columns via intuitive drag-and-drop functionality.</li>
+          <li><strong>Row Click to Edit</strong> – Open a modal with an edit form when clicking a row for quick updates.</li>
+          <li><strong>Search & Filter</strong> – Instantly find relevant data with a real-time search input.</li>
+        </ul>
+
+        <p>
+          This component is designed for <strong>performance, accessibility, and flexibility</strong>,
+          making it a powerful solution for handling complex data visualization in any React application. 🚀
+          Play around with the DataTable component, all changes are kept in <code>localStorage</code>.
+          You can add new users below by pressing the create button below or here <CreateUserButton />,
+          delete users and update users by clicking the row,
+          or reset data by clicking
+          <ConfirmButton
+            modalTitle='Reset user data'
+            buttonComponent={ResetButton}
+            onConfirm={() => setUsers(usersBase)}
+          />.
+        </p>
+        
         <DataTable
           orderByDefault='order'
           showEditModalOnClickRow
@@ -104,12 +128,28 @@ const DataTablePage = () => {
               selector: 'role',
               orderBy: 'role',
             },
+            {
+              name: 'Actions',
+              selector: (user) => (
+                <DeleteConfirmButton
+                  modalTitle={`Delete user "${user.name}?`}
+                  onDelete={() => {
+                    // Get index of item to delete
+                    const index = users.findIndex(u => u.id === user.id);
+                    // Remove from users list
+                    users.splice(index, 1)
+                    // Store users and make sure to update order field
+                    setUsers(users.map((u, order) => ({...u, order })));
+                  }}
+                />
+              )
+            }
           ]}
           data={users}
           onMove={({ item, target }) => {
             // Use onMove to store new position for instance by modifying the 'order' field
             // with django-ordered-model in a Django backend
-            
+
             // Find index of the item to move
             const fromIndex = users.findIndex(user => user.id === item.id);
             if (fromIndex === -1) return; // If not found, return original array
@@ -128,7 +168,7 @@ const DataTablePage = () => {
             setUsers(users.map((u, order) => ({...u, order })));
           }}
         />
-        <FormCreateModalButton title='Create new user'/>
+        <CreateUserButton />
       </FormModalProvider>
 
     </Container>
